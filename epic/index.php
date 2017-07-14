05fc4584806757634487f3592a6be1a5b012a878
Tim Williams <tikytimbo5@yahoo.com>
2017-07-14 09:03:07 -0600

    added title

---
+++ epic/index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Data Design Etsy Project</title>
+					<meta charset="utf8"/>
+	</head>
+	<body>
+		<h2> Persona, Use Case, Interaction Flow, and Conceptual Model </h2>
+		<p></p>
+	</body>
+</html>
