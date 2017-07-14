fa31ab51a54718ba322253d3a4449f789078f211
Tim Williams <tikytimbo5@yahoo.com>
2017-07-14 09:53:33 -0600

    added <p> to include persona and use case

--- epic/index.php
+++ epic/index.php
@@ -4,8 +4,32 @@
 		<title>Data Design Etsy Project</title>
 					<meta charset="utf8"/>
 	</head>
-	<body>
-		<h2> Persona, Use Case, Interaction Flow, and Conceptual Model </h2>
-		<p></p>
+	<body>,
+		<h1> Persona, Use Case, Interaction Flow, and Conceptual Model </h1>
+		<h2>Persona</h2>
+		<h3>Name:</h3><p>Robert Lester</p>
+
+		<h3>age:</h3>
+		<p>63</p>
+
+		<h3>profession:</h3>
+		<p>Robert is a commercial painter. He has been a blue collar worker his whole life and just recently retired.</p>
+
+		<h3>Technology</h3>
+		<p>Robert aims for top of the line products often buying without understanding the capabilities of the computer.
+			Operating system is windows. 4g smart phone with unlimited data. Reliable internet connection.</p>
+
+		<h3>Attitudes and behaviors</h3>
+		<p>Robert has no patience for technology, he typically asks his son for help. Always stays busy.</p>
+
+		<h3>Goals</h3>
+		<p>Robert has a goal of selling hand crafted art to a variety of people</p>
+
+		<h3>Story</h3>
+		<p>Robert,has had a hobby of making art from scratch and miscelanious things. Roberts family has encouraged him to sell his products to try and turn a profit.
+			Robert is looking for a web platform to sell his handcrafted art. Robert has little experience using the web and is looking for something easy to use with clear instructions on posting his work.</p>
+
+		<h2>Use Case</h2>
+		<p>Robert was reffered to etsy.com by his family and has read thru the instructions on how to upload his items to sell. Robert is leaving for vacation in 1 hour and would like to have his products up befor he leaves.</p>
 	</body>
 </html>
