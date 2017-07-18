8dea4e7daa1c2c8206010963115d31ed946526f9
Tim Williams <tikytimbo5@yahoo.com>
2017-07-18 11:56:57 -0600

    connected to data base

--- sql/etsy.sql
+++ sql/etsy.sql
@@ -2,9 +2,12 @@ DROP TABLE IF EXISTS product;
 DROP TABLE IF EXISTS profile;
 
 CREATE TABLE profile (
+
+
 		-- this creates the attribute for the primary key
 		-- auto_increment tells SQL to number them (1, 2, 3...)
 		-- not null means the attribute is required
+
 		profileId INT 	UNSIGNED AUTO_INCREMENT NOT NULL,
 		profileUsername VARCHAR(32) NOT NULL,
 		profileEmail VARCHAR(128) NOT NULL,
