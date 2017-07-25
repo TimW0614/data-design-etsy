194afd9d1b0c43ce3b95da3fa0bca3237da0debb
Tim Williams <tikytimbo5@yahoo.com>
2017-07-24 21:11:55 -0600

    added class item

--- epic/php/classes/item.php
+++ epic/php/classes/item.php
@@ -7,5 +7,23 @@ require_once("autoload.php");
  * This Item can be treated as a small example of what eCommerce websites like Contempo Design store when products are created using Contempo Design. This can follow suit for more features of Contempo Design.
  *
  * @author Timothy Williams <tkotalik@cnm.edu>
- * @version 1.0.0
+ * @version 4.0.1
  **/
+
+class Item {
+	/**
+	 * id for this Item; this is the primary key
+	 * @var int $itemId
+	 **/
+	private $itemId;
+	/**
+	 * id of the profile that saved this Item; this is a foreign key
+	 * @var int $itemProfileId
+	 **/
+	private $itemProfileId;
+	/**
+	 * actual content in text format that describes this Item
+	 * @var string $itemDescription
+	 **/
+
+}
