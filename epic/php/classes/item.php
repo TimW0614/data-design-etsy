17ea831b430f5b49eed1e0decbe4dab83b43a6a6
Tim Williams <tikytimbo5@yahoo.com>
2017-07-24 21:06:52 -0600

    started item.php

---
+++ epic/php/classes/item.php
@@ -0,0 +1,11 @@
+<?php
+namespace Edu\Cnm\DataDesign;
+require_once("autoload.php");
+/**
+ * Small Cross Section of a Contempo Design Product
+ *
+ * This Item can be treated as a small example of what eCommerce websites like Contempo Design store when products are created using Contempo Design. This can follow suit for more features of Contempo Design.
+ *
+ * @author Timothy Williams <tkotalik@cnm.edu>
+ * @version 1.0.0
+ **/
